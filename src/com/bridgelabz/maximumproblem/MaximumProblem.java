03b6e327c1f65a90aacc102a9613f41862ec56e4
VighneshBilgi <vhbilgi@gmail.com>
2022-05-22 20:35:01 +0530

    Added: UC2_Maximum_Float

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -8,17 +8,11 @@ public class MaximumProblem {
 
         System.out.println("Welcome to Maximum Problem");
 
-//        System.out.println("Enter 1st value:");
-//        Double val1 = sc.nextDouble();
-//        System.out.println("Enter 2nd value:");
-//        Double val2 = sc.nextDouble();
-//        System.out.println("Enter 3rd value:");
-//        Double val3 = sc.nextDouble();
-
         GenericClass genericClass = new GenericClass();
 
         System.out.println("The largest value is :"+genericClass.findMax(2,9,4));
         System.out.println("The largest value is :"+genericClass.findMax(0.84f,3.78f,9.83f));
+
 
     }
 }
