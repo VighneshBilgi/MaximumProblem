79acb27e8fbb35f79d3093475248bf1e42496d5e
VighneshBilgi <vhbilgi@gmail.com>
2022-05-22 19:44:15 +0530

    Added: Welcome to Maximum Problem

---
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.maximumproblem;
+
+import java.util.Scanner;
+
+public class MaximumProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Maximum Problem");
+
+    }
+}
